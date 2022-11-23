ac1c7165830402c12e8e2ef6e3fe4a3dbc054947
Rawllings Kiprop <rutojr01@gmail.com>
2022-11-23 15:24:49 +0300

    Incomplete functions lab

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Code your solution in this file!
+
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+function returnFirstTwoDrivers () {
+return drivers.slice(0,2);
+}
+
+console.log(returnFirstTwoDrivers());
+
+function returnLastTwoDrivers () {
+    return drivers.slice(-2);
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers] 
+
+
+function createFareMultiplier() {
+    return createFareMultiplier; 
+}
+const fareMultiplier = createFareMultiplier() * createFareMultiplier;
+
+
+
+function fareDoubler(fare) {
+    return fare * 2;
+}
+
+
+function fareTripler (fare) {
+    return fare * 3;
+}
+
+function selectDifferentDrivers () {
+    return returnFirstTwoDrivers();
+}
